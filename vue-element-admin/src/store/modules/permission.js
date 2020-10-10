f35afafe222345606170b154259a5e495921341e
Your Name <11091752@vivo.com>
2020-10-10 17:50:47 +0800

    添加动态功能

--- vue-element-admin/src/store/modules/permission.js
+++ vue-element-admin/src/store/modules/permission.js
@@ -1,4 +1,5 @@
 import { asyncRoutes, constantRoutes } from '@/router'
+import Layout from '@/layout'
 
 /**
  * Use meta.role to determine if the current user has permission
@@ -51,6 +52,27 @@ const actions = {
     return new Promise(resolve => {
       let accessedRoutes
       if (roles.includes('admin')) {
+        // 动态路由添加
+        const children = [
+          {
+            path: `/test/t/t1`,
+            component: () => import('@/views/test/index'),
+            meta: { title: '测试1', icon: 'edit' }
+          },
+          {
+            path: `/test/t/t2`,
+            component: () => import('@/views/test/index'),
+            meta: { title: '测试2', icon: 'edit' }
+          }
+        ]
+
+        asyncRoutes.unshift({
+          path: '/test/t',
+          component: Layout,
+          meta: { title: '测试', icon: 'documentation' },
+          children
+        }) // -- 动态路由结束
+
         accessedRoutes = asyncRoutes || []
       } else {
         accessedRoutes = filterAsyncRoutes(asyncRoutes, roles)
@@ -59,6 +81,20 @@ const actions = {
       resolve(accessedRoutes)
     })
   }
+
+
+  // generateRoutes({ commit }, roles) {
+  //   return new Promise(resolve => {
+  //     let accessedRoutes
+  //     if (roles.includes('admin')) {
+  //       accessedRoutes = asyncRoutes || []
+  //     } else {
+  //       accessedRoutes = filterAsyncRoutes(asyncRoutes, roles)
+  //     }
+  //     commit('SET_ROUTES', accessedRoutes)
+  //     resolve(accessedRoutes)
+  //   })
+  // }
 }
 
 export default {
